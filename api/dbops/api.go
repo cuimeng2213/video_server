a393ad46508c91edbafa022303fceaf0be74dd17
cuimeng2213 <270464169@qq.com>
2019-10-17 14:02:55 +0800

    add:增加数据层mysql连接和操作接口

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,17 +1,39 @@
 package dbops
+
 import (
-	"database/sql"
+	"fmt"
 )
 
+func AddUserCredential(loginName string, pwd string) error {
+	smtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES(?,?)")
 
-func openConn() *sqlDB{
-
+	if err != nil {
+		fmt.Printf("Add user error : %v\n", err)
+		return err
+	}
+	smtIns.Exec(loginName, pwd)
+	smtIns.Close()
+	return nil
 }
 
-func AddUserCredential( loginName string , pwd string) error {
-
+func GetUserCredential(loginName string) (string, error) {
+	smtOut, err := dbConn.Prepare("SELECT pwd FROM users WHERE login_name = ?")
+	if err != nil {
+		fmt.Printf("query pwd error: %v\n", err)
+		return "", err
+	}
+	var pwd string
+	smtOut.QueryRow(loginName).Scan(&pwd)
+	smtOut.Close()
+	return pwd, nil
 }
 
-func GetUserCredential(loginName string )(string, error){
-
+func DeletUser(loginName string, pwd string) error {
+	smtDel, err := dbConn.Prepare("DELETE FROM users where login_name=? and pwd = ?")
+	if err != nil {
+		return err
+	}
+	smtDel.Exec(loginName, pwd)
+	smtDel.Close()
+	return nil
 }
---
+++ api/dbops/api_test.go
@@ -0,0 +1,56 @@
+package dbops
+
+import (
+	"testing"
+)
+
+func clearDB() {
+	dbConn.Exec("truncate users")
+}
+
+func TestMain(m *testing.M) {
+	clearDB()
+	m.Run()
+	clearDB()
+}
+
+func TestApiFlow(t *testing.T) {
+	t.Run("ADD", testAddUser)
+	t.Run("Quert", testQueryUser)
+	t.Run("DEL", testDeleteUser)
+	t.Run("REGET", testReQueryUser)
+}
+
+func testAddUser(t *testing.T) {
+	err := AddUserCredential("tom", "12345")
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+	}
+}
+
+func testQueryUser(t *testing.T) {
+	pwd, err := GetUserCredential("tom")
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+	}
+	if pwd != "12345" {
+		t.Errorf("%v\n", "pwd error")
+	}
+}
+
+func testDeleteUser(t *testing.T) {
+	err := DeletUser("tom", "12345")
+	if err != nil {
+		t.Errorf("Delete error%v\n", err)
+	}
+}
+
+func testReQueryUser(t *testing.T) {
+	pwd, err := GetUserCredential("tom")
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+	}
+	if pwd != "" {
+		t.Errorf("%v\n", "pwd error")
+	}
+}
